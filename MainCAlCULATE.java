eed6f9fd046447e18372f6e43d03b96f14602bb1
NGUYENVANVINH <vinhqwertyuio8@gmail.com>
2024-10-08 13:57:12 +0700

    Add files via upload

---
+++ MainCAlCULATE.java
@@ -0,0 +1,19 @@
+
+package CALCULATION;
+
+
+
+
+public class MainCAlCULATE {
+
+
+
+     public static void main(String[] args) {
+        CALCULATE Calculate = new CALCULATE();
+        
+        
+    }
+    
+}
+     
+
